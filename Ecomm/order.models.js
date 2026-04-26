21c6d03ea4135773db09f378a1622af550598a23
Prakhar Jaiswal <prakharjaiswal221@gmail.com>
2026-04-26 14:12:09 +0530

    E-comm

---
+++ Ecomm/order.models.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const orderSchema = new mongoose.Schema({
+  orderPrice :{
+    type : String,
+    required : true
+  },
+  customer :{
+    type : mongoose.Schema.Types.ObjectId ,
+    ref : "User"
+  },
+  orderItems:{
+    type : []
+  }
+},{timestamps:true}) ;
+
+export const Order = mongoose.model('Order', orderSchema)
