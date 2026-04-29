cec8bef9f1386f8274f232397838a9458ec6d044
Prakhar Jaiswal <prakharjaiswal221@gmail.com>
2026-04-30 01:38:35 +0530

    api_resposes

---
+++ Industry_backend/src/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js"
+import connectDB from "../db/index.js";
+
+import dotenv from 'dotenv';
+import express from "express"
+
+const app = express()
+
+dotenv.config({ path: './env'})
+
+connectDB()
+.then(()=> {
+  app.listen(process.env.PORT || 8000 , ()=>{
+    console.log(`Server is running at port : ${process.env.PORT}`);
+    
+  })
+})
+.catch((err)=>{
+  console.log("MongoDB connection error !! ", err);
+  
+})
